8a73152ce212f3454893e289201a0e5f8cec3335
oja-bitterlife <bitter.life.club@gmail.com>
2025-06-06 22:57:57 +0900

    git管理開始

---
+++ komauchi/__init__.py
@@ -0,0 +1,1 @@
+from .komauchi import *
---
+++ komauchi/komauchi.py
@@ -0,0 +1,163 @@
+from krita import *
+from PyQt5.QtWidgets import QFileDialog, QMessageBox
+import csv, re
+
+FRAME_NO_MAX = 65536
+CELLS = ["A", "B", "C", "D"]
+
+# メッセージボックス
+def showInfo(title, str):
+    QMessageBox.information(
+        Krita.instance().activeWindow().qwindow(),
+        title,
+        str
+    )
+def showWarn(title, str):
+    QMessageBox.warning(
+        Krita.instance().activeWindow().qwindow(),
+        title,
+        str
+    )
+def showError(title, str):
+    QMessageBox.critical(
+        Krita.instance().activeWindow().qwindow(),
+        title,
+        str
+    )
+
+
+class MyExtension(Extension):
+
+    def __init__(self, parent):
+        # これは親クラスを初期化します。サブクラス化の際に重要です。
+        super().__init__(parent)
+        self.target_layers = {cell: {} for cell in CELLS }
+
+    def setup(self):
+        pass
+
+    def createActions(self, window):
+        action = window.createAction("oja_komauchi", "コマ打ち from CSV", "tools/scripts")
+        action.triggered.connect(self.import_csv)
+
+    def import_csv(self):
+        # 現在開いているファイル
+        doc = Krita.instance().activeDocument()
+        if not doc:
+            # ファイルを開いていない
+            showWarn("ドキュメントがありません", "先にドキュメントを開いてください")
+            return
+        doc_path = doc.fileName()
+
+        csv_file_path, _ = QFileDialog.getOpenFileName(Krita.instance().activeWindow().qwindow(), 
+                                                    "Select Komauchi CSV File", 
+                                                    doc_path,
+                                                    "CSV Files (*.csv);;All Files (*)")
+        if not csv_file_path:
+            showWarn("キャンセル", "実行を中止しました")
+            return
+
+
+        try:
+            with open(csv_file_path, 'r', encoding='utf-8') as f:
+                keyframes = []
+
+                for rows in csv.reader(f):
+                    # 1. 空行をスキップ
+                    if not rows:
+                        continue
+
+                    # 前後空白削除
+                    rows = [cell.strip() for cell in rows]
+
+                    # 空セルチェック
+                    if len(rows) == 0 or not rows[0]:
+                        continue
+
+                    # 2. コメント行をスキップ
+                    if rows[0].startswith('#'):
+                        continue
+
+                    # 3. 設定行の解析
+                    if rows[0].startswith('@'):
+                        # 設定の取得
+                        self.load_setting(rows)
+                        continue
+
+                    # 4. データ行
+                    frame_no = int(rows[0])
+                    if frame_no < 0 or frame_no >= FRAME_NO_MAX:
+                        raise Exception(f"FrameNo Error: {rows[0]}")
+
+                    # frame_noが入るようサイズを増やしておく
+                    if len(keyframes) <= frame_no:
+                        keyframes.extend([None] * (frame_no - len(keyframes) + 1))
+
+                    keyframes[frame_no] = [int(key) if key.isdigit() else None for key in rows[1:]]
+
+            # キーフレームを適用
+            self.apply_keyframes(doc, keyframes)
+
+            # 設定確認
+            # for k, v in self.settings.items():
+            #     showInfo(k, v)
+
+        except FileNotFoundError:
+            showError("エラー", f"CSVファイルを開けませんでした: {csv_file_path}")
+            return {}, []
+
+        except Exception as e:
+            showError("エラー", str(e))
+            return {}, []
+
+
+    # @設定の解析
+    def load_setting(self, rows):
+        m = re.match(r"@([A-Z]?)(\d?)$", rows[0])
+        if m:
+            cell,key = m.groups()[:2]
+            if cell and key:
+                self.target_layers[cell][key] = rows[1]
+                return
+
+        showWarn("未対応", f"未対応の設定です: {rows[0]}")
+
+
+    # キーフレームの設定
+    def apply_keyframes(self, doc, keyframes):
+        krita_layers = {}
+        # Kritaのレイヤーノードを再帰的に収集するヘルパー関数
+        def _collect_layers(node):
+            if node.nodeType() == 'grouplayer': # グループレイヤーの場合
+                for child in node.childNodes():
+                    _collect_layers(child) # 子ノードも再帰的に収集
+            elif node.nodeType() == 'clonelayer': # クローンレイヤだけ回収
+                krita_layers[node.name()] = node # レイヤー名をキー、レイヤーノードを値として格納
+        _collect_layers(doc.rootNode()) # ドキュメントのルートノードからレイヤー収集を開始
+
+
+        for i, keyframe in enumerate(keyframes):
+            # Frameスキップ
+            if keyframe is None:
+                continue
+
+            frame_no = i + 1
+
+            # Frameに含まれるキー
+            for j, keyframe_key in enumerate(keyframe):
+                if keyframe_key is None:
+                    # キーがあれば消去
+                    continue
+                cell = CELLS[j]
+
+                for target_key, target_layer_name in self.target_layers[cell].items():
+                    target_layer = krita_layers.get(target_layer_name)
+                    if target_layer is None:
+                        raise Exception(f"対象レイヤーが見つかりません: {target_layer_name}")
+
+                    target_layer.setAnimationFrameVisible(frame_no, keyframe_key == target_key)
+
+Krita.instance().addExtension(MyExtension(Krita.instance()))
+
+
+
